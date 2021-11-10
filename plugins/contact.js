4c57885d39b320b72613f3e15d1e314101907025
Romeo <90444506+ROMEO-7@users.noreply.github.com>
2021-11-10 06:12:49 +0530

    ❤

---
+++ plugins/contact.js
@@ -0,0 +1,17 @@
+const Ktb = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const exec = require('child_process').exec;
+const os = require("os");
+const fs = require('fs');
+const Config = require('../config')
+Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
+
+//coded by saidali
+const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+            + 'VERSION:3.0\n' 
+            + 'FN:ROMEO [OWNER]\n' // full name
+            + 'ORG:ROMEO;\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid=917306152493:+91 7306152493\n' // WhatsApp ID + phone number
+            + 'END:VCARD'
+await message.client.sendMessage(message.jid,{displayname: "ROMEO [OWNER]", vcard: vcard}, MessageType.contact)
+}))
