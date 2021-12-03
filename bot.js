b5b6b27c0f4b422b6212844961b733429bd5d9e0
Romeo <90444506+ROMEO-7@users.noreply.github.com>
2021-12-03 07:45:56 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,21 +110,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
     conn.on('open', async () => {
         console.log(
             chalk.green.bold('✅ Login successful!')
-        );
-        console.log(
-            chalk.blueBright.italic('Confirming password...')
-        );
-        if (config.AFPLK == 'AFAMK' || config.AFPLK == 'afamk' || config.AFPLK == 'Afamk' || config.AFPLK == 'pinky') {
-        //thanks to afnanplk
-        console.log(
-            chalk.green.bold('thanks for watching -key cofirmed-')
-        );
-         }
-         else if (config.AFPLK !== 'AFAMK' || config.AFPLK !== 'afamk' || config.AFPLK !== 'Afamk' || config.AFPLK !== 'pinky') {
-         console.log(
-            chalk.red.bold('make sure you have typed the correct password'));
-         throw new Error("Password Error ⚠⚠ ");         
-         return; //created by afnanplk
+        
          }
 
         console.log(
